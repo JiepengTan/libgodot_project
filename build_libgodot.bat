f8ea36f155dbef807b3daa88e6b3885a12ae5645
jiepengtan <jiepengtan@gmail.com>
2024-07-06 13:35:08 +0800

    set build flag to reduce dll size

--- build_libgodot.bat
+++ build_libgodot.bat
@@ -1,14 +1,16 @@
 set BASE_DIR=%~dp0
 cd %BASE_DIR%\godot
 
-call scons platform=windows arch=x86_64 verbose=yes msvc=yes dev_build=yes debug_symbols=yes
-call scons platform=windows target=template_debug arch=x86_64 verbose=yes msvc=yes library_type=shared_library dev_build=yes debug_symbols=yes
-
-copy /y bin\godot.windows.template_debug.dev.x86_64.dll ..\build\libgodot.dll
-
-start /wait bin\godot.windows.editor.dev.x86_64.exe --dump-extension-api
-
+REM Build editor and export extension API
+call scons platform=windows arch=x86_64 msvc=yes optimize=size
+start /wait bin\godot.windows.editor.x86_64.exe --dump-extension-api
 copy /y extension_api.json ..\build\extension_api.json
+
+REM Build libgodot
+call scons platform=windows arch=x86_64 msvc=yes library_type=shared_library optimize=size target=template_release
+copy /y bin\godot.windows.template_release.x86_64.dll ..\build\libgodot.dll
+
+REM Copy extension API and interface to godot-cpp
 copy /y ..\build\extension_api.json ..\godot-cpp\gdextension
 copy /y core\extension\gdextension_interface.h ..\godot-cpp\gdextension
 
